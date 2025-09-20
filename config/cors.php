02b307bcb25ca3a0202558851086c08ad642f717
Garou-ux <pahr9894.kf@gmail.com>
2025-09-20 01:14:11 -0600

    cors

--- config/cors.php
+++ config/cors.php
@@ -2,24 +2,15 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://simecproyectos.net',   // tu frontend en producción
+        'http://localhost:4200',        // si algún día pruebas con Angular/Vue
+        'http://127.0.0.1:8000'         // si pruebas API en local
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -32,3 +23,4 @@ return [
     'supports_credentials' => false,
 
 ];
+
