ae5a97eff5123fd1aba763ae30480643dab70a5a
Garou-ux <pahr9894.kf@gmail.com>
2026-05-16 17:25:04 -0600

    sasa

--- config/cors.php
+++ config/cors.php
@@ -2,15 +2,16 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'https://simecproyectos.net',
         'https://www.simecproyectos.net',
-        'http://localhost',
-        'http://127.0.0.1',
     ],
 
     'allowed_origins_patterns' => [
